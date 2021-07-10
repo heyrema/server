f866bfac43980cffd2e933603646894cce06352a
Param Siddharth <contact@paramsid.com>
2021-07-10 16:03:41 +0530

    Add database connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 /**
- * HIDE_DEBUG_OUTPUT: Any non-empty value implies no console output by Morgan
+ * HIDE_DEBUG_OUTPUT: Any non-empty value implies no console output by Morgan.
+ * PORT: The port on which Rema should run.
+ * DB: The MongoDB connection URI.
+ * NODE_ENV: The runtime environment, 'development' or 'production'.
  */
 require('dotenv').config();
 
@@ -12,25 +15,40 @@ const mongoose = require('mongoose');
 const { statusCode } = require('statushttp');
 
 const PORT = process.env.PORT ?? 8080;
+const DB = process.env.DB ?? `mongodb://localhost/rema`;
 
-const app = express();
+(async () => {
+	// Connect to database
+	try {
+		await mongoose.connect(DB, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log(`Connected to database. 🔐`);
+	} catch(e) {
+		console.log(`Failed to connect to database: "${e.message}"`);
+		process.exit(1);
+	}
 
-// Show debug output to console
-if (process.env.NODE_ENV !== 'production' || !process.env.HIDE_DEBUG_OUTPUT)
-	app.use(morgan('dev'));
+	const app = express();
 
-//
-app.use((req, res, next) => {
-	let poweredBy = res.getHeader('X-Powered-By');
-	if (poweredBy == null)
-		poweredBy = `Rema v${packageJson.version}`;
-	else
-		poweredBy = `Rema v${packageJson.version}, ${poweredBy}`;
-	
-	res.setHeader('X-Powered-By', poweredBy);
-	next();
-});
+	// Show debug output to console
+	if (process.env.NODE_ENV !== 'production' || !process.env.HIDE_DEBUG_OUTPUT)
+		app.use(morgan('dev'));
 
-app.all('*', (req, res) => res.send('Hi'));
+	// Powered by Rema :P
+	app.use((req, res, next) => {
+		let poweredBy = res.getHeader('X-Powered-By');
+		if (poweredBy == null)
+			poweredBy = `Rema v${packageJson.version}`;
+		else
+			poweredBy = `Rema v${packageJson.version}, ${poweredBy}`;
+		
+		res.setHeader('X-Powered-By', poweredBy);
+		next();
+	});
 
-app.listen(PORT, () => console.log(`Rema up on port ${PORT}. 😎`));
+	app.all('*', (req, res) => res.send('Hi'));
+
+	app.listen(PORT, () => console.log(`Rema up on port ${PORT}. 😎`));
+})();
