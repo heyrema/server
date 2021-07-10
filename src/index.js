bbda63ec17faec066ec7c2eb7ffcb5341207fe3c
Param Siddharth <contact@paramsid.com>
2021-07-10 16:31:44 +0530

    Add favicon

--- src/index.js
+++ src/index.js
@@ -17,38 +17,44 @@ const { statusCode } = require('statushttp');
 const PORT = process.env.PORT ?? 8080;
 const DB = process.env.DB ?? `mongodb://localhost/rema`;
 
+// For global await support
 (async () => {
-	// Connect to database
-	try {
-		await mongoose.connect(DB, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log(`Connected to database. 🔐`);
-	} catch(e) {
-		console.log(`Failed to connect to database: "${e.message}"`);
-		process.exit(1);
-	}
 
-	const app = express();
-
-	// Show debug output to console
-	if (process.env.NODE_ENV !== 'production' || !process.env.HIDE_DEBUG_OUTPUT)
-		app.use(morgan('dev'));
-
-	// Powered by Rema :P
-	app.use((req, res, next) => {
-		let poweredBy = res.getHeader('X-Powered-By');
-		if (poweredBy == null)
-			poweredBy = `Rema v${packageJson.version}`;
-		else
-			poweredBy = `Rema v${packageJson.version}, ${poweredBy}`;
-		
-		res.setHeader('X-Powered-By', poweredBy);
-		next();
+// Connect to database
+try {
+	await mongoose.connect(DB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
 	});
+	console.log(`Connected to database. 🔐`);
+} catch(e) {
+	console.log(`Failed to connect to database: "${e.message}"`);
+	process.exit(1);
+}
 
-	app.all('*', (req, res) => res.send('Hi'));
+const app = express();
 
-	app.listen(PORT, () => console.log(`Rema up on port ${PORT}. 😎`));
+// Show debug output to console
+if (process.env.NODE_ENV !== 'production' || !process.env.HIDE_DEBUG_OUTPUT)
+	app.use(morgan('dev'));
+
+// Powered by Rema :P
+app.use((req, res, next) => {
+	let poweredBy = res.getHeader('X-Powered-By');
+	if (poweredBy == null)
+		poweredBy = `Rema v${packageJson.version}`;
+	else
+		poweredBy = `Rema v${packageJson.version}, ${poweredBy}`;
+	
+	res.setHeader('X-Powered-By', poweredBy);
+	next();
+});
+
+// Static files
+app.use(express.static(path.join(__dirname, 'static', 'public')));
+
+app.all('*', (req, res) => res.send('Hi'));
+
+app.listen(PORT, () => console.log(`Rema up on port ${PORT}. 😎`));
+
 })();
