f14ea585fb2607687c9e0095343257b31a6ea010
Param Siddharth <contact@paramsid.com>
2021-07-15 01:41:01 +0530

    Update the value of MAX_CAIRO_DIMENSION to hardcoded 9830

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ const INTERNAL_STATIC_DIR = path.resolve(STATIC_DIR, 'internal');
 // Reducing to consume less memory, exceeding it will cause the rendered output to be scaled down
 const MAX_CAIRO_DIMENSION = (
 	(process.env.MAX_DIMENSION_OVERRIDE <= 32767 ? process.env.MAX_DIMENSION_OVERRIDE : null)
-	?? 32767 / (10 / 3)
+	?? 9830
 );
 
 const SINGLE_WHITE_PIXEL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
